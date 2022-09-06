f6ae933536b4a3647d2f580f122e454aa5ea5f32
LuisAlexisSalazar <kirito_SWO@outlook.com>
2022-09-06 12:02:58 -0500

    Matriz Global Tarea

--- Matriz Global/main.py
+++ Matriz Global/main.py
@@ -1,7 +1,8 @@
 from utils import *
 from Meddleman import Matrix
 
-# Press the green button in the gutter to run the script.
+# ?Link del simulador del algoritmo de Meddleman
+# https://bioboot.github.io/bimm143_W20/class-material/nw/
 if __name__ == '__main__':
     s1, s2 = readInput()
     MatrixMeddleman = Matrix(s1, s2, debug=False)
--- Matriz Global/utils.py
+++ Matriz Global/utils.py
@@ -1,5 +1,7 @@
 from itertools import groupby
-
+# https://bio.libretexts.org/Bookshelves/Computational_Biology/Book%3A_Computational_Biology_-_Genomes_Networks_and_Evolution_(Kellis_et_al.)/02%3A_Sequence_Alignment_and_Dynamic_Programming/2.05%3A_The_Needleman-Wunsch_Algorithm
+# Ejemplo del Link: AAGC AGT
+# Ejemplo Clase: AAAC AGC
 
 def readInput():
     with open('data/input.txt') as f:
@@ -13,10 +15,10 @@ def readInput():
 
     string_to_axis_y = (string_to_axis_y[:-1])
     string_to_axis_x = string_to_axis_x
-    print("Cadena 1:", string_to_axis_y)
-    print("Cadena 2:", string_to_axis_x)
     if len(string_to_axis_x) > len(string_to_axis_y):
         string_to_axis_y, string_to_axis_x = string_to_axis_x, string_to_axis_y
+    print("Cadena 1:", string_to_axis_y)
+    print("Cadena 2:", string_to_axis_x)
     return string_to_axis_y, string_to_axis_x
 
 
